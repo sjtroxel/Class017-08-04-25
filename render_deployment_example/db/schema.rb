9c84b2aaa0fd8d77541d62bcc66d926df40e8f7e
sjtroxel <sjtroxel@protonmail.com>
2025-08-01 13:00:55 -0500

    so frustrating cause I dunno even what a cable is

--- render_deployment_example/db/schema.rb
+++ render_deployment_example/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_31_195600) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_01_174915) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.datetime "created_at", null: false
