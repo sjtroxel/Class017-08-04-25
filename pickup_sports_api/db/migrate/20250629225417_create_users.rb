a6d6ccd1f4b86412be802d63931e2365206771d6
sjtroxel <sjtroxel@protonmail.com>
2025-08-02 11:39:15 -0500

    making adjustment to create users migration in respose to error deploying

--- pickup_sports_api/db/migrate/20250629225417_create_users.rb
+++ pickup_sports_api/db/migrate/20250629225417_create_users.rb
@@ -1,6 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[8.0]
   def change
-    create_table :users do |t|
+    create_table :users, if_not_exists: true do |t|
       t.string :username, null: false
       t.string :email, null: false
       t.string :first_name, null: false
